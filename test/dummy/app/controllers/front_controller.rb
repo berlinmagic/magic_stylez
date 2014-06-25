cb27b905f99f349617e6ba0a6de65ff456c5078f
Torsten Wetzel <torsten@orderlift.com>
2014-06-25 04:04:59 +0200

    update dummy app

--- test/dummy/app/controllers/front_controller.rb
+++ test/dummy/app/controllers/front_controller.rb
@@ -6,7 +6,11 @@ class FrontController < ApplicationController
   
   def templates
     Rails.logger.info "Front - templates"
-    render "templates/#{params[:template]}", layout: "blank"
+    if params[:template] && %w(responsive-slidebar fixed-header).include?(params[:template].to_s)
+      render "templates/#{params[:template]}", layout: "blank"
+    else
+      render "templates/responsive-slidebar", layout: "blank"
+    end
   end
   
 end
